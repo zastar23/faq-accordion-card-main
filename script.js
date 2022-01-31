aa0e063123c16888983dcb0425e34ced4005f051
zastar23 <florin.porut@gmail.com>
2022-01-31 20:26:39 +0200

    aesthetics next.

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 function showText() {
-	let text = (document.getElementById("invite__answer").innerHTML =
-		"You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.");
+	let text = document.getElementById("invite__answer");
+	if (text.style.display == "none") {
+		text.style.display = "block";
+	} else {
+		text.style.display = "none";
+	}
+}
 
-	showText();
+function showText1() {
+	let text1 = document.getElementById("upload__answer");
+	if (text1.style.display == "none") {
+		text1.style.display = "block";
+	} else {
+		text1.style.display = "none";
+	}
+}
+
+function showText2() {
+	let text2 = document.getElementById("password__answer");
+	if (text2.style.display == "none") {
+		text2.style.display = "block";
+	} else {
+		text2.style.display = "none";
+	}
+}
+
+function showText3() {
+	let text3 = document.getElementById("subscription__answer");
+	if (text3.style.display == "none") {
+		text3.style.display = "block";
+	} else {
+		text3.style.display = "none";
+	}
+}
+
+function showText4() {
+	let text4 = document.getElementById("support__answer");
+	if (text4.style.display == "none") {
+		text4.style.display = "block";
+	} else {
+		text4.style.display = "none";
+	}
 }
