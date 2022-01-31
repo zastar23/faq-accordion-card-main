7db36e7db7bd2dbc27755f8f8d7857cb7be91821
zastar23 <florin.porut@gmail.com>
2022-01-31 17:39:52 +0200

    Try again.

---
+++ script.js
@@ -0,0 +1,6 @@
+function showText() {
+	let text = (document.getElementById("invite__answer").innerHTML =
+		"You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.");
+
+	showText();
+}
